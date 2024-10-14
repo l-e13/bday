6f41dd7916626d7b3225828a644f985482b30108
l-e13 England <jww2fj@virginia.edu>
2024-10-14 17:32:51 +0000

    edits

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ def main():
 
     # Embed the personalized connections game
     st.subheader("Check out your Personalized Connections Game!")
-    st.components.v1.iframe("https://custom-connections-game.vercel.app/F5f5Siaz1RhixW9glhOi", height=600, width=800)
+    st.components.v1.iframe("https://custom-connections-game.vercel.app/F5f5Siaz1RhixW9glhOi", height=900, width=800)
 
 if __name__ == "__main__":
     main()
